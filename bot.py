a22916356083803f439e78a56faa0ef4090777c7
Luiz Fernando <62777781+LuizFV00@users.noreply.github.com>
2020-07-01 18:00:41 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
-from telegram.ext import Updater,CommandHandler
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+                         RegexHandler, ConversationHandler)
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 
 def welcome(update,context):
     message = 'Olá '+update.message.from_user.first_name +'!'
@@ -6,6 +8,35 @@ def welcome(update,context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
 
+
+def feedback(update, context):
+    message = 'Por favor, digite um feedback para o nosso tutorial:'
+    update.message.reply_text(message, reply_markup=ReplyKeyboardMarkup([], one_time_keyboard=True))
+    return STATE1
+
+
+def inputFeedback(update, context):
+    feedback = update.message.text
+    print(feedback)
+    if len(feedback) < 10:
+        message = """Seu feedback foi muito curtinho... 
+                        \nInforma mais pra gente, por favor?"""
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        return STATE1
+    else:
+        message = "Muito obrigada pelo seu feedback!"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+
+
+def inputFeedback2(update, context):
+    feedback = update.message.text
+    message = "Muito obrigada pelo seu feedback!"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+
+def cancel(update, context):
+    return ConversationHandler.END
 
 
 def main():
@@ -16,7 +47,14 @@ def main():
     updater.start_polling()
     print(str(updater))
     updater.idle()
-
+    conversation_handler = ConversationHandler(
+        entry_points=[CommandHandler('feedback', feedback)],
+        states={
+            STATE1: [MessageHandler(Filters.text, inputFeedback)],
+            STATE2: [MessageHandler(Filters.text, inputFeedback2)]
+        },
+        fallbacks=[CommandHandler('cancel', cancel)])
+    updater.dispatcher.add_handler(conversation_handler)
 
 
 
