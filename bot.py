77d1afb466d1631942595102abc8c2e65f4cb173
Luiz Fernando <62777781+LuizFV00@users.noreply.github.com>
2020-07-01 18:04:10 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import requests
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
                          RegexHandler, ConversationHandler)
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
